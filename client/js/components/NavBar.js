4530a4481a0e7876ade83c1e706fc1d3fbb7dc69
Anthony <awatson31911@gmail.com>
2019-09-16 19:16:46 -0400

    adds new nav design

--- client/js/components/NavBar.js
+++ client/js/components/NavBar.js
@@ -5,8 +5,12 @@ const NavBar = () => {
 
   return (
     <div className="navigation">
-      <input type="checkbox" className="navigation__checkbox id=" navi-toggle />
-      <label htmlFor="navi-toggle" className="navigation__button"></label>
+      <input type="checkbox" className="navigation__checkbox" id="navi-toggle" />
+      <label htmlFor="navi-toggle" className="navigation__button">
+        <span className="navigation__icon">
+        &nbsp;
+        </span>
+      </label>
       <div className="navigation__background">
         &nbsp;
       </div>
