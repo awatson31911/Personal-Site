83b4899ea4ba3ff499b674d662b8d7be9e95a30d
Anthony <awatson31911@gmail.com>
2019-09-24 16:57:55 -0400

    adds social conatact to contact section

--- client/js/components/Contact.js
+++ client/js/components/Contact.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
+import SocialBox from './SocialBox';
+
 const ContactForm = () => {
   return (
     <div className="contact">
+
       <div className="contact__text">
-        I am currently in the market for Full-Time work as either an engineer/developer or client-facing, technial contact. I&apos;m also always open to freelance opportunities, interesting projects or just a friendly &apos;How do you do?&apos;  
+        I am currently in the market for Full-Time work as either an engineer/developer or client-facing, technial contact. I&apos;m also always open to freelance opportunities, interesting projects or just a friendly &apos;How do you do?&apos;
       </div>
+
       <div className="contact__btn-container">
-        <a className="btn">Get in touch &rarr;</a>
+        <h1 className="contact__heading">Say goodday! &rarr;</h1>
+        <SocialBox />
       </div>
+
+
     </div>
   )
 }
